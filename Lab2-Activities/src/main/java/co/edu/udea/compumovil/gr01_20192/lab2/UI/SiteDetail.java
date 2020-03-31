ed371b9e78a0beff657373576ff7201f33b85398
Jose Andres Lopez Castaño <42095301+joselopez1913@users.noreply.github.com>
2020-03-30 23:57:55 -0500

    fragments with info of the items clicked listview

---
+++ Lab2-Activities/src/main/java/co/edu/udea/compumovil/gr01_20192/lab2/UI/SiteDetail.java
@@ -0,0 +1,33 @@
+package co.edu.udea.compumovil.gr01_20192.lab2.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import co.edu.udea.compumovil.gr01_20192.lab2.R;
+
+public class SiteDetail extends AppCompatActivity {
+
+    TextView site, desc, point;
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_view_poi);
+
+        image=findViewById(R.id.imagePOI22);
+        site=findViewById(R.id.txt11);
+        desc=findViewById(R.id.txt22);
+        point=findViewById(R.id.txt33);
+
+        Intent i=getIntent();
+        //image.setImageResource(i.getIntExtra("imagenes",0));
+        site.setText(i.getStringExtra("site"));
+        desc.setText(i.getStringExtra("desc"));
+        point.setText(i.getStringExtra("point"));
+    }
+}
